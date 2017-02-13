83c88bc48137c6be6467fa91c9132c6e7f4c6bbd
TasjaG <tasjagrigorieva@gmail.com>
2017-02-13 12:54:28 +0200

    Added several classes. Still needs the rest of the classes and annotations.

---
+++ annotations/src/main/java/metroteam/annotations/MetroStationClass.java
@@ -0,0 +1,47 @@
+package metroteam.annotations;
+
+public class MetroStationClass implements MetroStation {
+
+	private String stationName;
+	
+	public String getStationName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String setStationName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getStatistic() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int setStatistic(int statistic) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getStationID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setStationID() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void incrementStatistic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int countDangerRating() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
