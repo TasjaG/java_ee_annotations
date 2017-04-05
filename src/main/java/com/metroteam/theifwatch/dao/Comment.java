b6a8b9b6b5c56f3773302c8fd855aacec95b7dbc
hunter_yeva <evang.dnepr@gmail.com>
2017-04-05 13:42:42 +0300

    dao sample reating

---
+++ src/main/java/com/metroteam/theifwatch/dao/Comment.java
@@ -0,0 +1,59 @@
+package com.metroteam.theifwatch.dao;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="COMMENTS")
+public class Comment {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	public String getStationId() {
+		return stationId;
+	}
+
+	public void setStationId(String stationId) {
+		this.stationId = stationId;
+	}
+
+	@Column(name="stationId")
+	private String stationId;
+	
+	@Column(name="text")
+	private String text;
+	
+	@Column(name="author")
+	private String author;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+}
